0c33b4533fa89d33e913cfcefe1947689152837f
ProfessorFish0 <88455866+ProfessorFish0@users.noreply.github.com>
2021-12-15 18:26:58 +0000

    added base for verify and added apikey in the arguments for getReports function

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ import getReports from "./src/getReports.js"
 class Guardian{
   constructor(apiKey){
     this.key = apiKey
+    this.verified = false
+    this.verify()
   }
   getReports(){
-    getReports()
+    getReports(this.apiKey)
+  }
+  verify(){
+    //TODO: ADD VERIFY CODE TO VERIFY API KEY
+    this.verified = true || false
   }
 }
 export default Guardian
